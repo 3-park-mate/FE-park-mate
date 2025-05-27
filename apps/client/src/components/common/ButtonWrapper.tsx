20db73d65a899a8b5a71bd2aa0694560b5e8cc87
yeji <yeji020823@gmail.com>
2025-05-27 09:53:55 +0900

    [Feature] 즉시 예약 페이지 퍼블리싱 (+위치 검색 기능 추가) (#18)
    * feature: 캘린더 컴포넌트 제작 #14
    
    * feat: 위치 검색을 위한  카카오맵 API 연동 #16
    
    * feat: 위치 검색 기능 구현 #16
    
    * chore: 불필요한 코드 제거 #14
    
    * refactor: 빌드 오류 수정 및 불필요한 코드 제거 #3

---
+++ apps/client/src/components/common/ButtonWrapper.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@repo/ui/lib/utils';
+import React from 'react';
+
+export default function ButtonWrapper({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        'fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-[600px] px-5 pb-6 pt-8',
+        'bg-white/80 rounded-sm shadow-lg',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ apps/client/src/components/pages/reservation/ReservationInfoBox.tsx
@@ -0,0 +1,26 @@
+import { ReservationInfoBoxProps } from '@/types/reservationType';
+
+export function ReservationInfoBox({
+  boxName = '',
+  buttonName = '',
+  selected,
+  children,
+  onClick,
+}: ReservationInfoBoxProps) {
+  return selected ? (
+    <>
+      <div className="rounded-sm p-6 bg-white/60 shadow-md transition-all">
+        <h2 className="text-2xl font-semibold">{boxName}</h2>
+        {children}
+      </div>
+    </>
+  ) : (
+    <div
+      className="h-[50px] rounded-sm px-4 bg-white/60 flex justify-between items-center shadow-md"
+      onClick={onClick}
+    >
+      <p className="text-[16px] text-gray-2 font-bold">{boxName}</p>
+      <p>{buttonName}</p>
+    </div>
+  );
+}
