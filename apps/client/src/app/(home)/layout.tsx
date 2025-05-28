cc3f2acc9ead770d5638bc43f80d60024e324e6d
yejiis <yeji020823@gmail.com>
2025-05-28 17:52:31 +0900

    feat: 메인페이지 헤더 추가 #32

--- apps/client/src/app/(home)/layout.tsx
+++ apps/client/src/app/(home)/layout.tsx
@@ -1,6 +1,3 @@
-import NotificationButton from '@/components/common/NotificationButton';
-import ParkmateLogo from '@repo/ui/components/icon/ParkmateLogo';
-
 export default function layout({
   children,
 }: Readonly<{
@@ -8,10 +5,6 @@ export default function layout({
 }>) {
   return (
     <div className="min-h-screen relative bg-inner-background-gray">
-      <header className="px-5 flex justify-between pt-10 pb-11">
-        <ParkmateLogo />
-        <NotificationButton />
-      </header>
       {children}
     </div>
   );
--- apps/client/src/app/(home)/page.tsx
+++ apps/client/src/app/(home)/page.tsx
@@ -1,11 +1,20 @@
 import ActionSection from '@/components/pages/home/ActionSection';
 import MenuSection from '@/components/pages/home/MenuSection';
+import MapPinIcon from '@repo/ui/components/icon/MapPinIcon';
+import SearchIcon from '@repo/ui/components/icon/SearchIcon';
 
 export default function page() {
   return (
     <main>
-      <ActionSection />
-      <MenuSection />
+      <header className="px-5 flex justify-between bg-white pt-12 pb-6 items-center">
+        <div className="flex items-center space-x-2 font-semibold">
+          <MapPinIcon className="size-8" />
+          <p>서울 강남 코엑스 주변</p>
+        </div>
+        <SearchIcon className="size-7" />
+      </header>
+      {/* <ActionSection />
+      <MenuSection /> */}
     </main>
   );
 }
