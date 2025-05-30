5ab96b0e53a1fdb36b7af40c74e6f7652da4233b
yejiis <yeji020823@gmail.com>
2025-05-30 09:27:00 +0900

    feat: 실시간 위치 추적 함수 추가 #38

--- apps/client/src/utils/getCurrentLocationUtils.ts
+++ apps/client/src/utils/getCurrentLocationUtils.ts
@@ -7,6 +7,27 @@ export const getCurrentLocationUtils = (): Promise<{
       reject(new Error('Geolocation을 지원하지 않습니다.'));
     }
 
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        resolve({ latitude, longitude });
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+};
+
+export const watchLocationUtils = (): Promise<{
+  latitude: number;
+  longitude: number;
+}> => {
+  return new Promise((resolve, reject) => {
+    if (!navigator.geolocation) {
+      reject(new Error('Geolocation을 지원하지 않습니다.'));
+    }
+
     navigator.geolocation.watchPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
