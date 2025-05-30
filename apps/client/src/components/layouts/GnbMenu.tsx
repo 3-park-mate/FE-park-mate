820bd5881774a7ef3760b29a7c15f437c9f3b309
yejiis <yeji020823@gmail.com>
2025-05-30 11:05:40 +0900

    style: 하단 네비게이션바 y축 보정 추가 #42

--- apps/client/src/components/layouts/GnbMenu.tsx
+++ apps/client/src/components/layouts/GnbMenu.tsx
@@ -15,7 +15,7 @@ export function GnbMenu({ id, main = false, link, icon: Icon }: GnbMenuType) {
       <Link href={link}>
         <Icon
           className={cn(
-            'absolute -top-8 left-1/2 -translate-x-1/2 p-4 size-[4.375rem] rounded-3xl bg-primary outline-7 outline-white '
+            'absolute top-1 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 size-[4.375rem] rounded-3xl bg-primary outline-7 outline-white'
           )}
         />
       </Link>
@@ -26,7 +26,7 @@ export function GnbMenu({ id, main = false, link, icon: Icon }: GnbMenuType) {
         <Icon
           className={cn(
             'relative stroke-primary-dark-50 cursor-pointer',
-            isCurrent && 'ease-in stroke-navy-1 after:content size-[27px]'
+            isCurrent && 'ease-in stroke-navy-1 after:content'
           )}
         />
       </Link>
