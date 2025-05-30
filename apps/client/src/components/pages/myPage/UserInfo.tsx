b3f88755eba5c6f4167b14fef567ccdd00235e2e
unknown <doeh2580@gmail.com>
2025-05-30 16:43:28 +0900

    feat: 내 차량 페이지퍼블리싱 및  MyCar 컴포넌트 추가

--- apps/client/src/components/pages/myPage/UserInfo.tsx
+++ apps/client/src/components/pages/myPage/UserInfo.tsx
@@ -4,15 +4,15 @@ import { PaddedLayout } from '@repo/ui/components/common/CommonLayouts';
 export default function UserInfo() {
   return (
     <PaddedLayout className="bg-gray-light-1 py-8">
-      <p className="text-lg">
+      <p className="text-17px">
         <b>홍길동</b>님, 안녕하세요.
       </p>
       <p className="text-gray-3 text-sm">abc@naver.com</p>
       <div className="bg-white p-5 mt-5 rounded-t-lg">
-        <p className="text-gray-3">보유 포인트</p>
+        <p className="text-gray-3 text-15px">보유 포인트</p>
         <div className="flex justify-between">
-          <h2 className="text-[26px] font-semibold">12,000원</h2>
-          <Button className="text-base px-5 rounded-2xl">충전</Button>
+          <h2 className="text-2xl font-semibold">12,000원</h2>
+          <Button>충전</Button>
         </div>
       </div>
       <button className="w-full bg-white text-center rounded-b-lg border-t p-2 cursor-pointer">
---
+++ apps/client/src/components/pages/myPage/myCar/MyCarItem.tsx
@@ -0,0 +1,19 @@
+import { Car } from 'lucide-react';
+
+export default function MyCarItem() {
+  return (
+    <div className="outline outline-gray-1 rounded-lg px-4 py-3">
+      <div className="flex gap-1 items-center">
+        <p className="text-15px">차량별명</p>
+        <div className="h-[20px] text-primary-dark-50 border border-primary-dark-50 text-xs px-1 rounded-lg flex items-center">
+          기본
+        </div>
+      </div>
+      <div className="flex gap-1 items-center">
+        <Car fill="currentColor" className="text-gray-light-2" size={18} />
+        <p className="text-gray-dark-2 text-sm">123나 1234</p>
+      </div>
+      <button className="pt-2 text-sm text-gray-2 cursor-pointer">삭제</button>
+    </div>
+  );
+}
