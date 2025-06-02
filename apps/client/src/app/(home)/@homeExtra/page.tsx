6d638ca30f156870811cd3b3f3e4bf0e88d778f7
unknown <doeh2580@gmail.com>
2025-06-02 14:19:55 +0900

    feat: 홈 페이지 병렬 라우팅 기능 추가

---
+++ apps/client/src/app/(home)/@homeExtra/page.tsx
@@ -0,0 +1,11 @@
+import NearestParking from '@/components/pages/home/NearestParking';
+import ParkPointArea from '@/components/pages/home/ParkPointArea';
+
+export default function page() {
+  return (
+    <>
+      <ParkPointArea />
+      <NearestParking />
+    </>
+  );
+}
