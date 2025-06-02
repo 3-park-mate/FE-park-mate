ec4819768fb5c34723749721369da405278aa2e8
yejiis <yeji020823@gmail.com>
2025-06-02 09:21:38 +0900

    style: 하단 네비게이션바 지도 아이콘 크기 축소 #38

--- apps/client/src/components/layouts/GnbMenu.tsx
+++ apps/client/src/components/layouts/GnbMenu.tsx
@@ -15,7 +15,7 @@ export function GnbMenu({ main = false, link, icon: Icon }: GnbMenuType) {
       <Link href={link}>
         <Icon
           className={cn(
-            'absolute top-1 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 size-[4.375rem] rounded-3xl bg-primary outline-7 outline-white'
+            'absolute top-2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 size-[60px] rounded-3xl bg-primary outline-7 outline-white'
           )}
         />
       </Link>
