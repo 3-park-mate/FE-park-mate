eae4e3552c55fdf4b39288ed39e88624879ff8f1
YOON-YEJI <yeji020823@gmail.com>
2025-06-03 17:47:44 +0900

    feat: 메인 헤더 알림 컴포넌트 추가

--- apps/client/src/components/layouts/HomeMainHeader.tsx
+++ apps/client/src/components/layouts/HomeMainHeader.tsx
@@ -1,14 +1,16 @@
 import SearchIcon from '@repo/ui/components/icon/SearchIcon';
 import MarkerIcon from '@repo/ui/components/icon/MarkerIcon';
+import AlertBell from '../common/AlertBell';
 
 export default function HomeMainHeader() {
   return (
-    <header className="fixed top-0 w-full max-w-[600px] h-[65px] flex justify-between items-center bg-white p-5 shadow-md z-50">
-      <div className="flex items-center space-x-2">
+    <header className="fixed top-0 w-full max-w-[600px] h-[65px] flex justify-between items-center bg-white p-5 shadow-md space-x-[9px] z-50">
+      <div className="w-full flex items-center gap-2">
         <MarkerIcon className="size-6" />
         <p className="text-[0.813rem] font-semibold">서울 강남 코엑스 주변</p>
       </div>
-      <SearchIcon className="size-6.5" />
+      <SearchIcon className="size-[24px] flex-none" />
+      <AlertBell count={4} />
     </header>
   );
 }
