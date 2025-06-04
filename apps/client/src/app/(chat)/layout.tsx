5960f84a7d83f7e72de736ea8c6885895cda0946
yejiis <yeji020823@gmail.com>
2025-06-04 10:49:35 +0900

    feat: 채팅리스트 페이지 헤더에 뒤로가기 버튼 추가 #60

--- apps/client/src/app/(chat)/layout.tsx
+++ apps/client/src/app/(chat)/layout.tsx
@@ -1,11 +1,11 @@
-import GnbNavBar from '@/components/layouts/GnbNavBar';
 import HomeMainHeader from '@/components/layouts/HomeMainHeader';
 import { PaddedLayout } from '@repo/ui/components/common/CommonLayouts';
+import BackButton from '@/components/layouts/BackButton';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative flex flex-col min-h-screen">
-      <HomeMainHeader title="채팅" className="text-[1rem]" />
+      <HomeMainHeader title="채팅" className="text-[1rem]" Icon={BackButton} />
       <PaddedLayout className="py-25">{children}</PaddedLayout>
     </div>
   );
