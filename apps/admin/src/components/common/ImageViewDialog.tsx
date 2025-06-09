1975224e5a6a78e7045fb40593994435979210d0
unknown <doeh2580@gmail.com>
2025-06-09 14:52:30 +0900

    feat: ImageUploadInput을 컴포넌트로 분리 ImageUploadDropzone, ImageThumbnailList 추가 각각의 이미지 삭제 버튼 추가 이미지 상세보기 기능 추가

---
+++ apps/admin/src/components/common/ImageViewDialog.tsx
@@ -0,0 +1,39 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from '@repo/ui/components/base/dialog';
+import Image from 'next/image';
+
+export default function ImageViewDialog({
+  children,
+  title,
+  imgSrc,
+}: {
+  children: React.ReactNode;
+  title?: string;
+  imgSrc: string;
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild className="cursor-pointer">
+        {children}
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-white overflow-hidden border-none">
+        <DialogTitle className="text-center pt-4">{title}</DialogTitle>
+        <DialogDescription className="hidden" />
+        <div className="relative">
+          <Image
+            src={imgSrc}
+            alt="image-large"
+            width={800}
+            height={600}
+            className="object-contain max-h-[90vh] h-auto w-auto mx-auto"
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
