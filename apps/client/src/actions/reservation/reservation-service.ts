209184c622e3aeb59527cd2a1613b1305bb2d2bc
unknown <doeh2580@gmail.com>
2025-06-24 15:54:25 +0900

    feat: 예약 상세 조회 액션 및 타입 추가

---
+++ apps/client/src/actions/reservation/reservation-service.ts
@@ -0,0 +1,88 @@
+'use server';
+
+import { options } from '@/app/api/auth/[...nextauth]/options';
+import { api } from '@/hooks/serverFetch';
+import {
+  ReservationItemDataType,
+  ReservationListResponse,
+} from '@/types/reservationDataTypes';
+import { ApiResponse, CommonResponseType } from '@/types/responseDataTypes';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+const API_PREFIX = `${process.env.BASE_API_URL}/reservation-service/api/v1/reservations`;
+
+export async function getReservationsData({
+  size,
+  cursor,
+}: {
+  size: string;
+  cursor: string;
+}): Promise<ApiResponse<ReservationListResponse>> {
+  const payload = {
+    size,
+    cursor,
+  };
+  try {
+    const session = await getServerSession(options);
+    if (!session) {
+      redirect('/error');
+    }
+    const uuid = session.user.uuid;
+    const accessToken = session.user.accessToken;
+
+    const res = await api.get<CommonResponseType<ReservationListResponse>>(
+      API_PREFIX,
+      `/`,
+      payload,
+      {
+        headers: {
+          'X-User-UUID': `Bearer ${uuid}`,
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      }
+    );
+    console.log(res);
+
+    return {
+      success: true,
+      data: res.data,
+    };
+  } catch (_error) {
+    redirect('/error');
+  }
+}
+
+export async function getReservationDetailData(
+  reservationCode: string
+): Promise<ApiResponse<ReservationItemDataType>> {
+  try {
+    const session = await getServerSession(options);
+    if (!session) {
+      redirect('/error');
+    }
+    const uuid = session.user.uuid;
+    const accessToken = session.user.accessToken;
+    console.log('accessToken:', accessToken);
+
+    const res = await api.get<CommonResponseType<ReservationItemDataType>>(
+      API_PREFIX,
+      `/${reservationCode}`,
+      undefined,
+      {
+        headers: {
+          'X-User-UUID': uuid,
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      }
+    );
+    console.log(res);
+
+    return {
+      success: true,
+      data: res.data,
+    };
+  } catch (_error) {
+    redirect('/error');
+  }
+}
--- apps/client/src/app/(myPage)/my-reservations/[reservationCode]/page.tsx
+++ apps/client/src/app/(myPage)/my-reservations/[reservationCode]/page.tsx
@@ -1,3 +1,4 @@
+import { getReservationDetailData } from '@/actions/reservation/reservation-service';
 import PageHeader from '@/components/layouts/PageHeader';
 import ReservationDetail from '@/components/pages/myPage/myReservations/ReservationDetail';
 
@@ -6,7 +7,17 @@ export default async function page({
 }: {
   params: Promise<{ reservationCode: string }>;
 }) {
+  const fallback = <div>예약 정보를 찾을 수 없습니다.</div>;
+
   const { reservationCode } = await params;
+  if (!reservationCode) return fallback;
+  console.log('reservationCode:', reservationCode);
+
+  const res = await getReservationDetailData(reservationCode);
+  if (!res.success) return fallback;
+
+  const reservationData = res.data;
+  if (!reservationData) return fallback;
 
   return (
     <>
