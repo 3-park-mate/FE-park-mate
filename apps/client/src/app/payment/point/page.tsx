cd9e295ff507750f678ca6da3f449ebb83584654
unknown <doeh2580@gmail.com>
2025-06-26 17:01:34 +0900

    feat: 포인트 결제 페이지 퍼블리싱 추가

--- apps/client/src/app/payment/point/page.tsx
+++ apps/client/src/app/payment/point/page.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import PageHeader from '@/components/layouts/PageHeader';
+import ChargePointSection from '@/components/pages/payment/ChargePointSection';
+import {
+  CommonButton,
+  PaddedSection,
+} from '@repo/ui/components/common/CommonLayouts';
 
-export default function page() {
-  return <div>포인트 결제 페이지</div>;
+export default function Page() {
+  const currentPoints = 12000;
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <PageHeader title="포인트 충전" />
+
+      <main className="flex-1 flex flex-col justify-between">
+        <ChargePointSection />
+
+        <div className="bg-gray-light-1 shadow-md">
+          <PaddedSection className="py-5">
+            <div className="flex justify-between items-center pb-3">
+              <p className="text-sm text-gray-2">충전 후 보유 포인트</p>
+              <p className="text-2xl font-semibold">
+                {currentPoints.toLocaleString()}원
+              </p>
+            </div>
+            <CommonButton className="font-bold text-base text-black">
+              결제하기
+            </CommonButton>
+          </PaddedSection>
+        </div>
+      </main>
+    </div>
+  );
 }
