df71e8fec668e056190e12ae8e8613b5b06749c4
Chaeyeong <119782741+yunchyn@users.noreply.github.com>
2025-07-04 16:52:35 +0900

    [Fix] 전역 위치 관리 store에서 위치 구독 함수 삭제 (#178)
    * fix: 위치 감시 함수에서 오류가 발생하여 삭제
    
    * fix: 주차장 상세 리뷰 예외 처리

--- apps/client/src/components/common/LocationInitializer.tsx
+++ apps/client/src/components/common/LocationInitializer.tsx
@@ -4,15 +4,14 @@ import { useEffect } from 'react';
 import { useLocationStore } from '@/store/useLocationStore';
 
 export function LocationInitializer() {
-  const { latitude, longitude, fetchCurrentLocation, startWatchingLocation } =
-    useLocationStore();
+  const { latitude, longitude, fetchCurrentLocation } = useLocationStore();
 
   useEffect(() => {
     if (latitude == null || longitude == null) {
       fetchCurrentLocation();
-      startWatchingLocation();
+      // startWatchingLocation();
     }
-  }, [latitude, longitude, fetchCurrentLocation, startWatchingLocation]);
+  }, [latitude, longitude, fetchCurrentLocation]);
 
   return null;
 }
