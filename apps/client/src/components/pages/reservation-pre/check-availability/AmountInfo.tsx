0914b4df346de9bf4c866d94d87d55b3feb42bf4
yeji <yeji020823@gmail.com>
2025-07-09 13:36:37 +0900

    [Feature] 예약 페이지: 내 차량 연동 및 fallback 수정 (#204)
    * feat: 예약 사전 폼 전송 시 요청 값에 가격 정보 추가 #200
    
    * feat: 내 차량 선택 기능 추가 #202

--- apps/client/src/components/pages/reservation-pre/check-availability/AmountInfo.tsx
+++ apps/client/src/components/pages/reservation-pre/check-availability/AmountInfo.tsx
@@ -1,47 +1,26 @@
-import { getCaculatedTotalAmount } from '@/actions/parking/parking-service';
-import { useFetchData } from '@/hooks/useFetchData';
-import { CreateReservationRequestType } from '@/types/reservationDataTypes';
-import { ApiResponse } from '@/types/responseDataTypes';
 import DotSpinner from '@repo/ui/components/icon/DotSpinner';
-import { useCallback } from 'react';
-import { useFormContext } from 'react-hook-form';
 
-export default function AmountInfo() {
-  const { watch } = useFormContext<CreateReservationRequestType>();
-  const uuid = watch('parkingLotUuid');
-  const entryTime = watch('entryTime');
-  const exitTime = watch('exitTime');
-  const isScheduleSelected = entryTime && exitTime && uuid;
-
-  const fetcher = useCallback((): Promise<ApiResponse<{ amount: number }>> => {
-    if (!isScheduleSelected) {
-      return Promise.resolve({
-        success: false,
-        message: '일정 정보 없음',
-        data: null,
-      });
-    }
-    return getCaculatedTotalAmount({
-      parkingLotUuid: uuid,
-      startDateTime: entryTime,
-      endDateTime: exitTime,
-    });
-  }, [entryTime, exitTime, uuid, isScheduleSelected]);
-
-  const { data } = useFetchData<{ amount: number }>(fetcher);
-
+export default function AmountInfo({
+  amount,
+  isLoading,
+  isScheduleSelected,
+}: {
+  amount?: number;
+  isLoading: boolean;
+  isScheduleSelected: boolean;
+}) {
   return (
     <div>
       {isScheduleSelected ? (
         <>
           <p className="font-medium">총 결제금액</p>
-          {data ? (
+          {isLoading ? (
+            <DotSpinner className="mx-5" />
+          ) : (
             <p className="text-2xl font-bold">
-              {data?.amount.toLocaleString()}
+              {(amount ?? 0).toLocaleString()}
               <span className="text-xl font-semibold mx-0.5">원</span>
             </p>
-          ) : (
-            <DotSpinner />
           )}
         </>
       ) : (
